7e01391357b11c87b565fe5cec3e99e186aab5ba
Clare Radtke <clare.radtke@outlook.com>
2021-03-17 12:42:50 +1100

    HTML structure established, Quiz questions WIP

---
+++ assets/scripts/script.js
@@ -0,0 +1,26 @@
+// JS Fundamental questions:
+// Q: What are the 3 types of variable declaration you can use?
+//   A: var, let, const (correct)
+//   A: variable, declaration, let
+//   A: Var, Const, Dec
+//   A: var, declare, create
+
+// Q: JavaScript has six primitivedata types, they are:
+//   A: Boolean, String, Number, undefined, BigInt, Symbol (correct)
+//   A: Object, Function, Number, String, Boolean, NaN
+//   A: null, Function, Array, Boolean, String, Object
+//   A: Number, variable, Function, Array, Item, Declaration
+
+// Q: The following symbols +, =, !=, *, === are called what in JavaScript?
+//   A: Operators (correct)
+//   A: Data Types
+//   A: Variables
+//   A: Math symbols
+
+// Q: The if ... else statement commonly used in JavaScript is a
+//   A: Conditional (correct)
+//   A: Variable
+//   A: Function
+//   A: Object
+
+// Q:
