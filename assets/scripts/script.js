e6f305668278f936da4eaa72344d50c87cd0a709
Clare Radtke <clare.radtke@outlook.com>
2021-03-18 17:33:37 +1100

    User can now navigate from Title screen through each question to results and back to Title. Question selections are saved to session storage with the question index as a key.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,13 @@
 const questionEl = document.getElementById("question-text");
 const startBtnEl = document.getElementById("start-btn");
+const restartBtnEl = document.getElementById("restart-btn");
+const titleSection = document.querySelector(".title-container");
+const QnASection = document.getElementById("qNa");
+const resultsSection = document.getElementById("results");
 const INITIAL_DURATION = 5 * 60; // Sets the initial timer duration in seconds
+
+let currentQuestion;
+
 // Question array:
 const questions = [
   {
@@ -43,17 +50,90 @@ const questions = [
 
 // Display the Q&A container when start is clicked
 function dispQnA() {
-  document.getElementById("qNa").style = "display:block";
-  // Set the question to the Q&A p tag
-  document.getElementById("question-text").innerHTML = questions[0].question;
-
-  // Set the answers arrays and load to the li tags
-  // document.getElementById("A1").innerHTML = questions.question1.q1Answers[i];
-
-  // [0, 1, 2, 3] want to randomise this and set the random index to the li innerHTML
+  toggleQnADisplay();
+  // document.getElementById("qNa").style = "display:block";
+  currentQuestion = 0;
+  displayQuestion(currentQuestion);
 }
-startBtnEl.addEventListener("click", dispQnA);
-startBtnEl.addEventListener("click", startTimer);
+
+// When answer selected display next question and save the selection to session storage
+function displayNextQuestion(event) {
+  // Save the user selections to the session storage (to be displayed on results page)
+  // sessionStorage.setItem("key", "value")
+  let userSelections = window.sessionStorage;
+  userSelections.setItem(currentQuestion, event.target.id);
+  console.log("Session Storage object: ", userSelections);
+  currentQuestion++;
+  if (currentQuestion >= questions.length) {
+    toggleQnADisplay();
+    toggleResultsDisplay();
+  } else {
+    displayQuestion(currentQuestion);
+  }
+}
+
+// Run displayNextQuestion function when any answer li is clicked
+document.querySelectorAll(".answer").forEach(function (element) {
+  element.addEventListener("click", displayNextQuestion);
+});
+
+// Render questions to Q&A Section
+function displayQuestion(index) {
+  // Set the question to the Q&A p tag
+  questionEl.innerHTML = questions[index].question;
+  // Set the answers array and load to the li tags
+  document.getElementById("A1").innerHTML = questions[index].answers[0];
+  document.getElementById("A2").innerHTML = questions[index].answers[1];
+  document.getElementById("A3").innerHTML = questions[index].answers[2];
+  document.getElementById("A4").innerHTML = questions[index].answers[3];
+}
+
+// When start button clicked start timer, hide the Title section & display the Quiz
+startBtnEl.addEventListener("click", startQuiz);
+
+// combined all start button click events
+function startQuiz() {
+  dispQnA();
+  startTimer();
+  toggleTitleDisplay();
+}
+
+// Return to the Title section & hide everything else when Restart button clicked
+restartBtnEl.addEventListener("click", returnHome);
+
+// combined the restart button click events
+function returnHome() {
+  toggleTitleDisplay();
+  toggleResultsDisplay();
+}
+
+//Hide or show the title section
+function toggleTitleDisplay() {
+  if (titleSection.style.display === "none" || "") {
+    titleSection.style.display = "block";
+  } else {
+    titleSection.style = "display:none";
+  }
+}
+
+// Hide or show the Q&A Section
+function toggleQnADisplay() {
+  if (QnASection.style.display === "none" || "") {
+    QnASection.style.display = "block";
+  } else {
+    QnASection.style = "display:none";
+  }
+}
+
+//Hide or show Results display
+function toggleResultsDisplay() {
+  if (resultsSection.style.display === "none" || "") {
+    resultsSection.style.display = "block";
+  } else {
+    resultsSection.style = "display:none";
+  }
+}
+
 //TIMER CODE
 let timeCountDownEl = document.getElementById("time-counter");
 let timer;
@@ -80,8 +160,8 @@ function startTimer() {
     }
   }, 1000);
 }
-
+// Display results when timer reaches 0
 function timesUp() {
-  document.getElementById("qNa").style = "display:none";
-  document.getElementById("results").style = "display:block";
+  toggleQnADisplay();
+  toggleResultsDisplay();
 }
