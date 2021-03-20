f6b19b70b2328cff97dd6770908ce575296071e5
Clare Radtke <clare.radtke@outlook.com>
2021-03-20 15:15:53 +1100

    chore: renamed functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,23 @@
+//TODO:
+// stop timer when Q&ASection display=none
+// timer minus 30 sec when wrong answer
+// save remaining time to session storage
+// calculate score
+// display score in results
+// display remaining time in results
+// list out the Q and answers in results
+// identify the correct answer and the selected answer in results
+// save score, initials, remaining time to highscore list
+// display highscore list
+// decide how to access highscore list
+
 const questionEl = document.getElementById("question-text");
 const startBtnEl = document.getElementById("start-btn");
 const restartBtnEl = document.getElementById("restart-btn");
 const titleSection = document.querySelector(".title-container");
 const QnASection = document.getElementById("qNa");
 const resultsSection = document.getElementById("results");
-const INITIAL_DURATION = 5 * 60; // Sets the initial timer duration in seconds
+const INITIAL_DURATION = 2 * 60; // Sets the initial timer duration in seconds
 
 let currentQuestion;
 
@@ -48,16 +61,8 @@ const questions = [
   },
 ];
 
-// Display the Q&A container when start is clicked
-function dispQnA() {
-  toggleQnADisplay();
-  // document.getElementById("qNa").style = "display:block";
-  currentQuestion = 0;
-  displayQuestion(currentQuestion);
-}
-
 // When answer selected display next question and save the selection to session storage
-function displayNextQuestion(event) {
+function showNextQuestion(event) {
   // Save the user selections to the session storage (to be displayed on results page)
   // sessionStorage.setItem("key", "value")
   let userSelections = window.sessionStorage;
@@ -65,20 +70,20 @@ function displayNextQuestion(event) {
   console.log("Session Storage object: ", userSelections);
   currentQuestion++;
   if (currentQuestion >= questions.length) {
-    toggleQnADisplay();
-    toggleResultsDisplay();
+    qnaShow();
+    resultsShow();
   } else {
-    displayQuestion(currentQuestion);
+    showQuestion(currentQuestion);
   }
 }
 
 // Run displayNextQuestion function when any answer li is clicked
 document.querySelectorAll(".answer").forEach(function (element) {
-  element.addEventListener("click", displayNextQuestion);
+  element.addEventListener("click", showNextQuestion);
 });
 
 // Render questions to Q&A Section
-function displayQuestion(index) {
+function showQuestion(index) {
   // Set the question to the Q&A p tag
   questionEl.innerHTML = questions[index].question;
   // Set the answers array and load to the li tags
@@ -92,10 +97,14 @@ function displayQuestion(index) {
 startBtnEl.addEventListener("click", startQuiz);
 
 // combined all start button click events
+// Display the Q&A container when start is clicked
 function startQuiz() {
-  dispQnA();
+  qnaShow();
+  // document.getElementById("qNa").style = "display:block";
+  currentQuestion = 0;
+  showQuestion(currentQuestion);
   startTimer();
-  toggleTitleDisplay();
+  titleHide();
 }
 
 // Return to the Title section & hide everything else when Restart button clicked
@@ -103,35 +112,32 @@ restartBtnEl.addEventListener("click", returnHome);
 
 // combined the restart button click events
 function returnHome() {
-  toggleTitleDisplay();
-  toggleResultsDisplay();
+  titleShow();
+  resultsHide();
 }
 
 //Hide or show the title section
-function toggleTitleDisplay() {
-  if (titleSection.style.display === "none" || "") {
-    titleSection.style.display = "block";
-  } else {
-    titleSection.style = "display:none";
-  }
+function titleShow() {
+  titleSection.style.display = "block";
+}
+function titleHide() {
+  titleSection.style.display = "none";
 }
 
 // Hide or show the Q&A Section
-function toggleQnADisplay() {
-  if (QnASection.style.display === "none" || "") {
-    QnASection.style.display = "block";
-  } else {
-    QnASection.style = "display:none";
-  }
+function qnaShow() {
+  QnASection.style.display = "block";
+}
+function qnaHide() {
+  QnASection.style.display = "none";
 }
 
 //Hide or show Results display
-function toggleResultsDisplay() {
-  if (resultsSection.style.display === "none" || "") {
-    resultsSection.style.display = "block";
-  } else {
-    resultsSection.style = "display:none";
-  }
+function resultsShow() {
+  resultsSection.style.display = "block";
+}
+function resultsHide() {
+  resultsSection.style.display = "none";
 }
 
 //TIMER CODE
@@ -162,6 +168,6 @@ function startTimer() {
 }
 // Display results when timer reaches 0
 function timesUp() {
-  toggleQnADisplay();
-  toggleResultsDisplay();
+  qnaHide();
+  resultsShow();
 }
